c9ffa7e160e24e3ab5edbf0b0045969f588053d6
LeYangNwpu <nwpuyangle@gmail.com>
2018-09-10 17:30:07 +0800

    traversal binary tree

---
+++ tree/traversal_binary_tree.py
@@ -0,0 +1,32 @@
+class Node:
+	def __init__(self, key):
+		self.left = None
+		self.right = None
+		self.value = key
+
+def printInorder(root):
+    if root:
+        printInorder(root.left)
+        print(root.value)
+        printInorder(root.right)
+
+def printPreorder(root):
+    if root:
+        print(root.value)
+        printPreorder(root.left)
+        printPreorder(root.right)
+
+def printPostorder(root):
+    if root:
+        printPostorder(root.left)
+        printPostorder(root.right)
+        print(root.value)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+printPostorder(root)
+
